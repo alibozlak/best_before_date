2f150034ffde788a618cbee56a54b4f112b88b68
Ali Bozlak <bozlak20aliihsan@gmail.com>
2026-02-27 12:56:01 +0300

    First commit for bbd project

---
+++ src/main/java/dev/bozlak/bbd/BbdApplication.java
@@ -0,0 +1,13 @@
+package dev.bozlak.bbd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BbdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BbdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/bozlak/bbd/BbdApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.bozlak.bbd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BbdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
